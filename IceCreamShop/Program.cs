493ae053cd4cc5c581c65d6f856b4e6287e73cee
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-15 11:54:50 -0400

    Final changes to Exercise

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace IceCreamShop
 {
@@ -11,13 +13,51 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
+            //Console.WriteLine($"Before:");
+            //foreach (Flavor flavor in availableFlavors)
+            //{
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            //    Console.WriteLine(flavor.Name);
+            //}
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            //Console.WriteLine("");
+
+            availableFlavors.Sort(new FlavorComparer());
+            Console.WriteLine($"After: ");
+
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+
+
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+
+            //Console.WriteLine($"Before:");
+            //foreach (Cone cone in availableCones)
+            //{
+
+            //    Console.WriteLine(cone.Cost);
+            //}
+
+            //Console.WriteLine("");
+
+            availableCones.Sort(new ConeComparer());
+            Console.WriteLine($"After: ");
+
+            foreach (Cone cone in availableCones)
+            {
+                Console.WriteLine(cone.Cost);
+            }
+
+
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+
         }
     }
 }
